8fc97f74073aaa570f2cc27fa34f220867e4a32f
ange0814 <anatoly.gerasimov@netcracker.com>
2017-11-13 16:04:33 +0400

    add credentials

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
-
-firebase.initializeApp({
-    'messagingSenderId': '448358493027'
-});
+importScripts('https://www.gstatic.com/firebasejs/4.6.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.6.2/firebase-messaging.js');
 
 firebase.messaging();
 
